1850779c2a82d506efe12a6ab0c0255325b53d55
Daniel Cook <danielecook@gmail.com>
2018-03-09 21:37:09 -0600

    update rebuild

--- rebuild_envs.sh
+++ rebuild_envs.sh
@@ -21,6 +21,8 @@ for environment_name in primary py2; do
     pyenv uninstall -f ${environment_name}-${DATE}
     pyenv virtualenv --force ${CONDA_VERSION} ${environment_name}-${DATE}
     pyenv local ${environment_name}-${DATE}
-    conda env update --name=${environment_name}-${DATE} --file=${environment_name}.environment.yaml
+    conda env update --name ${environment_name}-${DATE} --file ${environment_name}.environment.yaml
     conda env export --name ${environment_name}-${DATE} > versions/${machine}.${DATE}.environment.yaml
 done;
+
+rm .python-version
