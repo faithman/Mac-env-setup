9b2a0474e7852dd370fef02a9daaf4f876196f6b
Daniel Cook <danielecook@gmail.com>
2018-03-09 21:23:52 -0600

    Fix typo

--- rebuild_envs.sh
+++ rebuild_envs.sh
@@ -21,6 +21,6 @@ for environment_name in primary py2; do
     pyenv uninstall -f ${environment_name}-${DATE}
     pyenv virtualenv --force ${CONDA_VERSION} ${environment_name}-${DATE}
     pyenv local ${environment_name}-${DATE}
-    conda env update --name=${environment_name}-${DATE} --file=${environment_name}.enviornment.yaml
+    conda env update --name=${environment_name}-${DATE} --file=${environment_name}.environment.yaml
     conda env export --name ${environment_name}-${DATE} > versions/${machine}.${DATE}.environment.yaml
 done;
